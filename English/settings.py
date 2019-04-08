e01b24b2f9a4bc6d236c4fa4a49661689e240853
yancx <yancx@walimaker.com>
2019-04-08 19:56:27 +0800

    mysql

--- English/settings.py
+++ English/settings.py
@@ -48,12 +48,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'English.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'127.0.0.1',      #主机端口、用户密码、数据库
+        'PORT':3306,
+        'USER':'root',
+        'PASSWORD':'root',
+        'NAME': 'English',
+
     }
 }
+# }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
